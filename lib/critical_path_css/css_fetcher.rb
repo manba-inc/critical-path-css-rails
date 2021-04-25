d11ea41b6a51ac46e9175b453355d2358aab5d1a
masayuki oguni <masayuki-oguni@manba.co.jp>
2021-04-25 11:50:46 +0900

    Add fetch_direct.

--- lib/critical_path_css/css_fetcher.rb
+++ lib/critical_path_css/css_fetcher.rb
@@ -51,5 +51,44 @@ module CriticalPathCss
       end
       out
     end
+
+    def fetch_direct(route)
+      options = {
+        'url' => @config.base_url + route,
+        'css' => @config.css_paths.first,
+        'width' => 1300,
+        'height' => 900,
+        'timeout' => 30_000,
+        # CSS selectors to always include, e.g.:
+        'forceInclude' => [
+          #  '.keepMeEvenIfNotSeenInDom',
+          #  '^\.regexWorksToo'
+        ],
+        # set to true to throw on CSS errors (will run faster if no errors)
+        'strict' => false,
+        # characters; strip out inline base64 encoded resources larger than this
+        'maxEmbeddedBase64Length' => 1000,
+        # specify which user agent string when loading the page
+        'userAgent' => 'Penthouse Critical Path CSS Generator',
+        # ms; render wait timeout before CSS processing starts (default: 100)
+        'renderWaitTime' => 100,
+        # set to false to load (external) JS (default: true)
+        'blockJSRequests' => true,
+        'customPageHeaders' => {
+          # use if getting compression errors like 'Data corrupted':
+          'Accept-Encoding' => 'identity'
+        }
+      }.merge(@config.penthouse_options)
+      out, err, st = Dir.chdir(GEM_ROOT) do
+        Open3.capture3('node', 'lib/fetch-css.js', JSON.dump(options))
+      end
+      if !st.exitstatus.zero? || out.empty? && !err.empty?
+        STDOUT.puts out
+        STDERR.puts err
+        STDERR.puts "Failed to get CSS for route #{route}\n" \
+              "  with options=#{options.inspect}"
+      end
+      out
+    end
   end
 end
